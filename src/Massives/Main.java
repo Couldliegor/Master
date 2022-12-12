de92803af38474415d7e9ee112bf240e9cc3a6ab
egorcouldlie <couldliegor@gmail.com>
2022-12-12 13:16:32 +0300

    Exercise 1

--- src/Massives/Main.java
+++ src/Massives/Main.java
@@ -2,6 +2,17 @@ package Massives;
 
 public class Main {
     public static void main(String[] args) {
-
+        task1();
     }
+
+    public static void task1() {
+        //task 1
+        int[] massive = new int[] {1,2,3};
+        // or massive[0] = 1;
+        // . massive[1] = 2;
+        //  massive[2] = 3;
+        float[] massiveFloat = {1,57, 7,654, 9,986};
+        double[]massiveDouble = new double[]{1 , 23,6 , 78,8990 };
+    }
+    //done
 }
